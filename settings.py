01afe73acd73684b319a4f1454e9c8694a159960
ugo <ugo.bertello@nuvoair.com>
2022-10-28 16:06:51 +0100

    Move 1 up.

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 import os
 
@@ -7,3 +8,7 @@ trues = ['true', 't']
 
 ENVIRONMENT = os.environ['ENVIRONMENT']
 DEBUG = os.getenv('DEBUG').lower() if (os.getenv('DEBUG') and os.getenv('DEBUG') in trues) else False
+
+ROOT_PATH = Path(__file__).parent.absolute()
+APP_PATH = ROOT_PATH
+APP_STATIC_PATH = os.path.join(APP_PATH, 'static')
