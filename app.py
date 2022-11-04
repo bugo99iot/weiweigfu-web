c5e4dcc5b1d25550a1f0f11d2004c88e5a34719f
ugo <ugo.bertello@nuvoair.com>
2022-11-04 16:04:40 +0000

    Added layout and working on contact forms.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os.path
 import random
 
 from flask import Flask, render_template
+from flask import request
+
 from settings import DEBUG
 from settings import FLASK_APP_SECRET_KEY
 from settings import APP_PATH, APP_STATIC_PATH
@@ -19,29 +21,39 @@ app.config.update(APPLICATION_ROOT=APP_PATH,
 @app.route("/", methods=["GET"])
 def home():
     test_image = os.path.join('static', 'images', 'math_art.jpg')
-    return render_template('core/home.html', test_image=test_image)
+    return render_template('core/home.html', test_image=test_image, title='WeiWeiGFU')
 
 
-@app.route("/contact", methods=["GET", "POST"])
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
-    contact_form = ContactForm()
-    if contact_form.validate_on_submit():
-        print(f"Name:{contact_form.name.data}, "
-              f"E-mail:{contact_form.email.data}, "
-              f"message:{contact_form.message.data}")
-    return render_template("core/contact.html", form=contact_form)
+    form = ContactForm()
+    if form.validate_on_submit():
+        print(f"Name:{form.name.data}, E-mail:{form.email.data}, message: {form.message.data}")
+
+    return render_template("core/contact.html", form=form)
 
 
 @app.route("/meaning-of-life", methods=["GET"])
 def meaning_of_life():
     meaning_of_life_choice: str = random.choice(ProjectData.MEANING_OF_LIFE)
-    return render_template('installations/meaning_of_life.html', meaning_of_life=meaning_of_life_choice)
+    return render_template('installations/meaning_of_life.html',
+                           title='Meaning of Life',
+                           meaning_of_life=meaning_of_life_choice)
 
 
 @app.route("/what-do-you-do", methods=["GET"])
 def what_do_you_do():
     what_do_you_do_choice: str = random.choice(ProjectData.WHAT_DO_YOU_DO)
-    return render_template('installations/what_do_you_do.html', what_do_you_do=what_do_you_do_choice)
+    return render_template('installations/what_do_you_do.html',
+                           title='What do you do',
+                           what_do_you_do=what_do_you_do_choice)
+
+
+@app.route("/child", methods=["GET"])
+def child():
+    return render_template('test/child.html',
+                           title='Child',
+                           description="Smarter page templates with Flask & Jinja.")
 
 
 if __name__ == "__main__":
--- utils/forms.py
+++ utils/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
 
 class ContactForm(FlaskForm):
     name = StringField(label='Name', validators=[DataRequired()])
-    email = StringField(label='Email', validators=[DataRequired(), Email(granular_message=True)])
-    message = StringField(label='Message')
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    message = TextAreaField(label='Message', validators=[DataRequired()])
     send = SubmitField(label="Send")
